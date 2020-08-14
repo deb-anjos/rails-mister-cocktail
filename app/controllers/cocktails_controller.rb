8a7a58f3c09847e8abfcfe97002743a869bb0cd4
deb-anjos <debora_anjos@icloud.com>
2020-08-14 12:10:08 -0400

    redeploying heroku

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @ingredient = Ingredient.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    new_cocktail = Cocktail.create(cocktail_params)
+    redirect_to new_cocktail_dose_path(new_cocktail)
+
+    # new_cocktail = Cocktail.new(cocktail_params)
+    # if new_cocktail.save
+    #   redirect_to new_cocktail_dose_path(new_cocktail)
+    # else
+    #   render :new
+    # end
+  end
+
+private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, :cocktail, :ingredient, presence: true
+  validates :description, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 
 end
